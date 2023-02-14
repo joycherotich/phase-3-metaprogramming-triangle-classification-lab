867e6effe21abd15207efade3d3b37ba272b7209
joy <letimjoy7@gmail.com>
2023-02-14 13:52:42 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+
 class Triangle
   # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a, @b, @c = a, b, c
+    validate_triangle
+  end
+
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    if a <= 0 || b <= 0 || c <= 0 || (a + b <= c) || (b + c <= a) || (a + c <= b)
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
